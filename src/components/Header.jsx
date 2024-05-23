7bb6c852265ffb0053a08f4c25caf2b20df5f683
Rhasley Biângulo <rhasley.biangulo@gmail.com>
2024-05-23 11:14:41 -0400

    Init App

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import style from "./Header.module.css";
+
+export const Header = () => {
+    return (
+        <header className={style.header}>
+            <h1>R Feed</h1>
+        </header>
+    );
+};
